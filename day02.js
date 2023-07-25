0d816ce5f211775b093f008dbbe0fba22457a387
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-24 19:35:37 -0700

    Add files via upload

---
+++ day02.js
@@ -0,0 +1,78 @@
+"use strict";
+// This exercise is to assign different data types on variables.
+// let prop01 = "Chair";
+// let qty01 = 2;
+// let inUse = true;
+// console.log(prop01);
+// console.log(qty01);
+// console.log(inUse);
+// This exercise is to check data (typeof) on variables.
+// let card01 = "Wedding Ceremony";
+// let qty02 = 500;
+// let isPrint = true;
+// console.log(typeof card01);
+// console.log(typeof qty02);
+// console.log(typeof isPrint);
+// This exercise is to assign personal data type into variables.
+// let car01:String = "Peugeot";
+// let numCC:number = 2000;
+// console.log(car01);
+// console.log(numCC);
+// This exercise is to assign concatenate between multiple variables.
+// let car02 = "Kia Sportage";
+// let numCC02 = 1999;
+// console.log("My " + car02 + " engine displacement is " + numCC02 + "cc.");
+// This exercise is to assign template literals in console.
+// let name05 = "Abraham Lincoln";
+// let message05 = "Whatever you are, be a good one";
+// console.log(`${name05} once said,\n"${message05}".`);
+// This exercise is to assign arithmetic operators in variables.
+// let packs01 = 25;
+// let frnd01 = 3;
+// let frnd02 = 5;
+// console.log(packs01 / frnd02); // 5
+// console.log(packs01 * frnd01); // 75
+// console.log(packs01 + frnd02); // 30
+// console.log(packs01 - frnd02); // 20
+// console.log(packs01 % frnd01); // 1
+// This exercise is to assign assignment operators in variables
+// let kid01 = 5;
+// let kid02 = 3;
+// let kid03 = 2;
+// console.log(kid01 ** kid03);    // 25
+// console.log(kid02 += kid01);    // 8
+// console.log(kid03 -= kid01);    // -3
+// This exercise is to assign comparison operators.
+// let marks01 = 85;
+// let grade01 = "A";
+// console.log(marks01 === 77);   //false
+// console.log(grade01 === "A");  //true
+// console.log(marks01 != 85);    //false
+// console.log(grade01 < "C");    //true
+// console.log(marks01 > 90);     //false
+// console.log(grade01 <= "B");   //true
+// console.log(marks01 >= 89);    //false
+// This exercise is to assign logical operators.
+// let userName01 = "assadsid";
+// let password01 = 123456;
+// let answer01 = [userName01 === "assadsid" && password01 === 12346];
+// let answer02 = [userName01 === "assadsidd" || password01 === 123456];
+// let answer03 = ![userName01 === "assadsid" && password01 === 123456];
+// console.log(`${answer01}\n${answer02}\n${answer03}`);
+// This exercise is to assign if, else & else if conditions.
+// let score1 = 95;
+// if (score1 > 90) {
+//     console.log("Grade A");
+//     if (score1 >= 95) {
+//         console.log("Win Gold Trophy");
+//     }
+// } else if (score1 > 80) {
+//     console.log("Grade B");
+//     if (score1 >= 85 && score1 < 90) {
+//       console.log("Win Silver Trophy");
+//     }
+// } else if (score1 > 60 && score1 < 80) {
+//     console.log("Passed");
+// } else {
+//   console.log("Failed!!")
+// }
