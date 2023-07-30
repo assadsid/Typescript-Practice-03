9b4e440f3e47d59fbd59272ec8f7aa7c2c900c6a
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-31 00:08:11 +0500

    Add files via upload

---
+++ day03.js
@@ -0,0 +1,93 @@
+"use strict";
+//Personal Message;
+// let name06 = "Eric";
+// console.log(`Hello ${name06},\nwould you like to learn some "Python" today?`);
+// Name Cases;
+// let name07 = "Hassan";
+// console.log(`${name07.toLowerCase()}\n${name07.toUpperCase()}\n${name07}`);
+// Famous Quote;
+// let name08 = "M.A. Jinnah";
+// console.log(`${name08} once said,\n"There is no Power in Earth that can undo Pakistan"`);
+// Famous Quote 2;
+// let famous_person = "M.A. Jinnah";
+// let message_fp = '"There is no Power in Earth that can undo Pakistan"';
+// console.log(`${famous_person} once said,\n${message_fp}`);
+// Stripping Names;
+// let name09 = "Haris";
+// let name09b = "Rauf";
+// console.log(`\t${name09},\n ${name09b}\t.`);
+// Number Eight;
+// console.log(16/2);
+// console.log(2*4);
+// console.log(6+2);
+// console.log(9-1);
+// Favourite Number;
+// let fav_num = 8;
+// let message_fn = `When you two multiply by four, the answer is ${fav_num}.`;
+// console.log(message_fn);
+// Adding Comment;
+/*
+This program is decribe famous quote directly in console
+Famous Quote;
+let name08 = "M.A. Jinnah";
+console.log(`${name08} once said,\n"There is no Power in Earth that can undo Pakistan"`);
+
+This program is decribe famous quote in two variable
+Famous Quote 2;
+let famous_person = "M.A. Jinnah";
+let message_fp = '"There is no Power in Earth that can undo Pakistan"';
+console.log(`${famous_person} once said,\n${message_fp}`);
+*/
+// Names;
+// let friends_arr = ["Hassan", "Haris", "Ali", "Sarwar", "Amir"];
+// friends_arr.forEach((Element) => {
+//     console.log(Element);
+// })
+// Greetings;
+// let friends_arr = ["Hassan", "Haris", "Ali", "Sarwar", "Amir"];
+// friends_arr.forEach((Element) => {
+//     console.log(`${Element}, would you like to join the party tonight?`);
+// })
+// Own Array;
+// let myOwnArr = ["Swift", "Cuore", "Corolla GLI"];
+// console.log(`I have a ${myOwnArr[0]} automatic car`);
+// console.log(`${myOwnArr[1]} is a small, but a fastest car`);
+// console.log(`${myOwnArr[2]} have a maximum seating capacity`);
+// if, nested if, else if, else examples;
+// let score02 = 95;
+// if(score02 >= 90) {
+//     console.log("Grade A+");
+//     if(score02 >= 95) {
+//         console.log("Reward $200");
+//     }
+// } else if(score02 >= 80) {
+//     console.log("Grade A");
+// } else if(score02 >= 70) {
+//     console.log("Grade B")
+// }else if(score02 >= 60) {
+//     console.log("Grade C")
+// } else {
+//     console.log("Failed");
+// }
+// Guest List;
+// let people_arr = ["Arman", "Aqib", "Uncle"];
+// for(let item of people_arr) {
+//     console.log(`Dear ${item}, I'd like to invite you for dinner tonight`);
+// }
+// Changing Guest List;
+// let people_arr = ["Arman", "Aqib", "Uncle"];
+// for(let item of people_arr) {
+//     console.log(`Dear ${item}, come to join a dinner tonight`);
+// }
+// console.log(`Sadly, ${people_arr[2]} will not join us for some reason`);
+// people_arr[2] = "Haris";
+// for(let item of people_arr) {
+//     console.log(`Dear ${item}, come to join a dinner tonight`);
+// }
+// For Loop Examples
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
